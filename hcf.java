2e281ebb8ef23451d75b27a2fafa3271f73a8d73
Sneha Shree <snehashree2501@gmail.com>
2022-02-17 21:42:05 +0530

    HCF basic code

---
+++ hcf.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class hcf {
+    public static void main(String[] args) {
+        //basic solution
+
+        Scanner s = new Scanner(System.in) ;
+               
+        System.out.println("Enter 2 numbers");
+         int a=s.nextInt();
+         int b=s.nextInt();
+
+         int hcf=Math.min(a, b);
+         while (hcf>0)
+         {
+             if (a%hcf==0 && b%hcf==0)
+                break;
+            hcf--;
+         }
+         System.out.println("HCF: "+hcf);
+    }
+
+}
